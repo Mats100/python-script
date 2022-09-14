59f818572564350490d69a0ab820bba6e90450fb
Mats100 <muneeb75@icloud.com>
2022-09-14 17:45:31 +0500

    addded exception

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-try:
-    number = int(input("Display multiplication table of? "))
-    if number:
+is_true = True
+while is_true == True:
+    try:
+        number = int(input("Display multiplication table of? "))
         for i in range(1, 11):
             print(number, 'x', i, '=', number * i)
-except:
-    print("It's a string.You must enter an integer")
+        break
+    except:
+        is_true == False
+        print("It's a string.You must enter an integer")
