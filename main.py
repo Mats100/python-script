4d43e4b21265f25a54f02b87d73c122d7f991084
Mats100 <muneeb75@icloud.com>
2022-09-14 17:49:39 +0500

    code simplified

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-is_true = True
-while is_true == True:
+while True:
     try:
         number = int(input("Display multiplication table of? "))
         for i in range(1, 11):
             print(number, 'x', i, '=', number * i)
         break
     except:
-        is_true == False
         print("It's a string.You must enter an integer")
